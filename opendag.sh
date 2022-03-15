017dc6b9f4c5e9324c560aa359a27289f317d8c7
Tijmen Muller <tijmen.muller@hu.nl>
2022-03-15 12:28:04 +0100

    Small improvements.

--- opendag.sh
+++ opendag.sh
@@ -13,24 +13,28 @@ sudo dpkg-reconfigure --front-end noninteractive locales
 
 echo Updating packages...
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 sudo apt-get dist-upgrade
+sudo apt autoremove
 
-echo Installing extra packages
+echo Installing extra packages...
 sudo apt-get install dselect
 sudo apt-get install firefox-esr
+sudo apt-get install python3-pil.imagetk
 
 echo Installing Python libraries...
 pip install pygame
 pip install imdbpy
 pip install requests
+pip install termcolor
 
 echo Downloading demonstraton projects...
 
-mkdir ~/workspace
+mkdir -p ~/workspace
 cd ~/workspace
 
 git clone https://github.com/tijmenjoppe/IPASS_MovieEnjoymentPredictor
 git clone https://github.com/tijmenjoppe/IPASS_MazeGame
 git clone https://github.com/tijmenjoppe/IPASS_Rummi
 git clone https://github.com/tijmenjoppe/IPASS_VierOpEenRij
+git clone https://github.com/tijmenjoppe/IPASS_Scrabble
