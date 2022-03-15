657ca7b06c2a6a60aaf4eee53d25b5cdbd5e8eea
Tijmen Muller <tijmen.muller@hu.nl>
2022-03-15 10:05:01 +0100

    Added updating of packages.
    * Installing firefox.
    * Non-interactive updating of locales.

--- opendag.sh
+++ opendag.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Bash script om demo's te installeren op een Raspberry Pi
+# Bash script to install demo projects on Linux
 # Tijmen Muller (tijmen.muller@hu.nl)
 
+# Exit on error
+set -e
+
 echo Updating locales...
-sudo dpkg-reconfigure locales
+sudo update-locale "LANG=en_US.UTF-8"
+sudo locale-gen --purge "en_US.UTF-8"
+sudo dpkg-reconfigure --front-end noninteractive locales
 
 echo Updating packages...
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
 
 echo Installing extra packages
 sudo apt-get install dselect
-sudo apt-get
+sudo apt-get install firefox-esr
 
 echo Installing Python libraries...
 pip install pygame
