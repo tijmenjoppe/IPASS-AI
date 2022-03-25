3e43e15bb543d6dadf25bb1dee6f931f8bbab4d3
Tijmen Muller <tijmen.muller@hu.nl>
2022-03-25 15:13:52 +0100

    Added NKepler project.

--- opendag.sh
+++ opendag.sh
@@ -6,21 +6,25 @@
 # Exit on error
 set -e
 
+
 echo Updating locales...
-sudo update-locale "LANG=en_US.UTF-8"
-sudo locale-gen --purge "en_US.UTF-8"
-sudo dpkg-reconfigure --front-end noninteractive locales
+# sudo update-locale "LANG=en_US.UTF-8"
+# sudo locale-gen --purge "en_US.UTF-8"
+# sudo dpkg-reconfigure --front-end noninteractive locales
+
 
 echo Updating packages...
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get dist-upgrade
-sudo apt autoremove
+sudo apt -y autoremove
+
 
 echo Installing extra packages...
 sudo apt-get install dselect
 sudo apt-get install firefox-esr
 sudo apt-get install python3-pil.imagetk
+
 
 echo Installing Python libraries...
 pip install pygame
@@ -28,13 +32,17 @@ pip install imdbpy
 pip install requests
 pip install termcolor
 
-echo Downloading demonstraton projects...
 
+echo Downloading demonstration projects...
 mkdir -p ~/workspace
 cd ~/workspace
 
-git clone https://github.com/tijmenjoppe/IPASS_MovieEnjoymentPredictor
+git clone https://github.com/tijmenjoppe/IPASS_BattleshipAI
+git clone https://github.com/tijmenjoppe/IPASS_CatanAI
 git clone https://github.com/tijmenjoppe/IPASS_MazeGame
+git clone https://github.com/tijmenjoppe/IPASS_MovieEnjoymentPredictor
+git clone https://github.com/tijmenjoppe/IPASS_NKepler
 git clone https://github.com/tijmenjoppe/IPASS_Rummi
-git clone https://github.com/tijmenjoppe/IPASS_VierOpEenRij
 git clone https://github.com/tijmenjoppe/IPASS_Scrabble
+git clone https://github.com/tijmenjoppe/IPASS_SudokuMagic
+git clone https://github.com/tijmenjoppe/IPASS_VierOpEenRij
